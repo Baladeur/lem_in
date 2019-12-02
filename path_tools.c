a050887a5e60d46292c3c0d411d35d67c770e52d
Theo FERRIEUX <tferrieu@e3r9p11.42.fr>
2019-12-02 20:28:11 +0100

    WIP Path(s) tools

--- path_tools.c
+++ path_tools.c
@@ -3,32 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   path_tools.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tferrieu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tferrieu <tferrieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 15:32:18 by tferrieu          #+#    #+#             */
-/*   Updated: 2019/11/29 15:32:19 by tferrieu         ###   ########.fr       */
+/*   Updated: 2019/12/02 20:27:44 by tferrieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-t_path			*destroy_path(t_path **path)
+t_path	*destroy_path(t_path **path)
 {
-	if ((*path)->nodes)
-		free((*path)->nodes);
-	if ((*path)->edges)
-		free((*path)->edges);
-	free(*path);
-	*path = NULL;
+	if (*path)
+	{
+		if ((*path)->nodes)
+			free((*path)->nodes);
+		if ((*path)->edges)
+			free((*path)->edges);
+		free(*path);
+		*path = NULL;
+	}
 	return (NULL);
 }
 
-t_path			*new_path(int *edges, int count)
+t_path	*destroy_paths(t_paths **paths)
+{
+	t_path *current;
+	t_path *tmp;
+
+	if (!paths || !(*paths))
+		return (NULL);
+	current = *paths;
+	*paths = NULL;
+	while (current)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+	}
+	return (NULL);
+}
+
+t_path	*new_path(int *edges, int count)
 {
 	t_path	*path;
 	int		i;
 
-	if (edges == NULL || !(path = (t_path *)malloc(sizeof(t_path))))
+	path = NULL;
+	if (!edges || count < 2 || !(path = (t_path *)malloc(sizeof(t_path))))
 		return (NULL);
 	path->edges = edges;
 	path->nodes = NULL;
@@ -49,6 +71,48 @@ t_path			*new_path(int *edges, int count)
 	return (path);
 }
 
+t_paths	*new_paths(t_path *path, int count)
+{
+	t_paths	*paths;
+	int		i;
+
+	if (count < 2 || !path || !(paths = (t_paths *)malloc(sizeof(t_paths))))
+		return (NULL);
+	paths->count = NULL;
+	paths->next = NULL;
+	paths->path = path;
+	if (!(paths->nodes = (int *)malloc(sizeof(int) * count)))
+		return (destroy_paths(&paths));
+	if (!(paths->count = (int *)malloc(sizeof(int))))
+		return (destroy_paths(&paths));
+	i = -1;
+	while (++i < count)
+		paths->nodes[i] = path->nodes[i];
+	paths->count[0] = 1;
+	return (paths);
+}
+
+t_paths	*add_path(t_paths *paths, t_path *path, int count)
+{
+	t_paths	*new;
+	int i;
+
+	if (!path || !paths || !(new = (t_paths *)malloc(sizeof(t_paths))))
+		return (NULL);
+	new->nodes = paths->nodes;
+	new->count = paths->count;
+	new->count[0]++;
+	new->path = path;
+	new->next = NULL;
+	i = -1;
+	while (++i < count)
+		if (path->nodes[i])
+			new->nodes[i] = 1;
+	paths->next = new;
+	return (paths);
+}
+
+/*
 void			print_path(t_path *path, int count)
 {
 	int i;
@@ -68,3 +132,4 @@ void			print_path(t_path *path, int count)
 		ft_printf(" %d", path->nodes[i]);
 	ft_printf("\n\nPath length :\t%d\n", path->len);
 }
+*/
