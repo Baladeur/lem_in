3c1ed7f4844e96274c2d3f457be2ae8c9b47b58b
Marie-shirine YENER <myener@e3r10p14.42.fr>
2020-01-22 15:58:55 +0100

    22012020_1558_mae

--- dispatcher.c
+++ dispatcher.c
@@ -6,10 +6,33 @@
 /*   By: myener <myener@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 17:22:11 by myener            #+#    #+#             */
-/*   Updated: 2020/01/21 17:54:09 by myener           ###   ########.fr       */
+/*   Updated: 2020/01/22 15:58:29 by myener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
+void	dispatcher(t_info *info, t_path *path_tab, t_room *room)
+{
+	int		a;	// current ant
+	int		i;	// index
+	int		n;	// path_tab index
+	char	*room_name;
 
+	i = 0;
+	a = 1;	// as stipulated in the subject, ant IDs start at 1.
+	n = 1;	// 1 and not 0 because path_tab[i].edges[0] = the start room
+	room_name = NULL;
+	while (path_tab[i].len != -1)
+	{
+		if (room_is_empty(path_tab[i].edges[n])) // goes thru ant arr. to see if an ant's here
+		{
+			info->ant[a].id = path_tab[i].edges[n];
+			room_name = get_room_from_id(path_tab[i].edges[n], room);
+			ft_printf("L%d-%s ", a, room_name);
+			a++;
+			n++;
+		}
+		i++;
+	}
+}
--- displayer.c
+++ displayer.c
@@ -6,7 +6,7 @@
 /*   By: myener <myener@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 16:40:45 by myener            #+#    #+#             */
-/*   Updated: 2020/01/21 18:02:15 by myener           ###   ########.fr       */
+/*   Updated: 2020/01/22 15:58:55 by myener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,5 @@ void	displayer(t_paths *path, t_info *info, char **map)
 		i++;
 	}
 	ft_putchar('\n'); // one additional space (needed in the examples)
-	i = 1; // i starts at 1 cause the ants id's start at 1.
-	ant_last_pos = 0; // at first, all ants start at 0
-	while (i < info->ant_nb) // while we go through all the ants
-	{
-
-		if (info->ant_position[i] != ant_last_pos) // if the ant position is now different
-		ft_printf("L%d-%s ", /*ant_number*/, /*room_name*/);
-	}
-	ft_putchar('\n'); // go to next line for the next iteration
+	dispatcher()
 }
