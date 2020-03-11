5f3fbdc990c658b5d936df6b671c3b0dd4204704
Maeril <Maeril@Avesta.local>
2020-03-11 16:59:24 +0100

    11032020_1659_mae

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: myener <myener@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/22 15:20:24 by myener            #+#    #+#             */
-/*   Updated: 2020/03/11 16:45:10 by myener           ###   ########.fr       */
+/*   Updated: 2020/03/11 16:57:25 by myener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,68 @@
 
 static int	room_parser(char *line, t_info *info, int j)
 {
-	int 	i;
-	int		end;
+	int		i;
+	int		start;
 	char	*tmp;
 
 	i = 0;
 	tmp = NULL;
-	info->room_tab[j].id = j;
-	while (line[i]) // get to the end of the line
+	while (line[i] && line[i] != ' ')
 		i++;
-	i--; // goes right before \0
-	end = i; // stocks the last character's position (and end of y)
-	while (line[i] != ' ') // while we didn't encounter a space, we go back up
-		i--;
-	// tmp = ft_strsub(line, i, end);
-	if (!lem_in_atoi(line, i, end, &info->room_tab[j].y))
+	info->room_tab[j].id = j;
+	info->room_tab[j].name = ft_strsub(line, 0, i);
+	i++;
+	start = i;
+	while (line[i] && line[i] != ' ')
+		i++;
+	tmp = ft_strsub(line, start, i);
+	if (!lem_in_atoi(tmp, &info->room_tab[j].x))
 		return (0);
-	// tmp ? free(tmp) : 0;
-	i--; // jumps over the space
-	end = i; // stocks the theoretical end of x
-	while (line[i] != ' ') // while we didn't encounter a space, we go back up
-		i--;
-	// tmp = ft_strsub(line, i, end);
-	// info->room_tab[j].x = ft_atoi(tmp);
-	if (!lem_in_atoi(line, i, end, &info->room_tab[j].x))
+	tmp ? free(tmp) : 0;
+	start = i++;
+	while (line[i] && line[i] != ' ')
+		i++;
+	tmp = ft_strsub(line, start, i);
+	if (!lem_in_atoi(tmp, &info->room_tab[j].y))
 		return (0);
-	// tmp ? free(tmp) : 0;
-	i--;
-	end = i; // stocks the theoretical end of name
-	info->room_tab[j].name = ft_strsub(line, 0, end + 1); // everything before x and y can supposedly be the name so we grab it all.
+	tmp ? free(tmp) : 0;
 	return (1);
 }
+
+// static int	room_parser(char *line, t_info *info, int j)
+// {
+// 	int 	i;
+// 	int		end;
+// 	char	*tmp;
+
+// 	i = 0;
+// 	tmp = NULL;
+// 	info->room_tab[j].id = j;
+// 	while (line[i]) // get to the end of the line
+// 		i++;
+// 	i--; // goes right before \0
+// 	end = i; // stocks the last character's position (and end of y)
+// 	while (line[i] != ' ') // while we didn't encounter a space, we go back up
+// 		i--;
+// 	// tmp = ft_strsub(line, i, end);
+// 	if (!lem_in_atoi(line, i, end - i, &info->room_tab[j].y))
+// 		return (0);
+// 	// tmp ? free(tmp) : 0;
+// 	i--; // jumps over the space
+// 	end = i; // stocks the theoretical end of x
+// 	while (line[i] != ' ') // while we didn't encounter a space, we go back up
+// 		i--;
+// 	// tmp = ft_strsub(line, i, end);
+// 	// info->room_tab[j].x = ft_atoi(tmp);
+// 	if (!lem_in_atoi(line, i, end - i, &info->room_tab[j].x))
+// 		return (0);
+// 	// tmp ? free(tmp) : 0;
+// 	i--;
+// 	end = i; // stocks the theoretical end of name
+// 	info->room_tab[j].name = ft_strsub(line, 0, end + 1); // everything before x and y can supposedly be the name so we grab it all.
+// 	return (1);
+// }
+
 
 // static int	room_parser(char *line, t_info *info, int j)
 // {
@@ -138,13 +169,13 @@ int			lem_in_parser(char **map, t_info *info)
 			if ((ret = hash_line_manager(map, i)) > 1)
 				if (!gates_manager(info, ret, i, map)
 					&& (info->edges_line = i) >= 0)
-					return (room_add_start_end(map, info) ? 1 : 0);
+					break;
 			i += (ret == 1) ? 0 : 1;
 		}
 		else if (map[i][0] >= 33 && map[i][0] <= 126 && (j = (j == 0) ? 1 : j))
 			if (j += is_room(map[i]) == 1)
 				if (!room_parser(map[i], info, j))
-					return (room_add_start_end(map, info) ? 1 : 0);
+					break;
 	}
 	return (room_add_start_end(map, info) ? 1 : 0);
 }
