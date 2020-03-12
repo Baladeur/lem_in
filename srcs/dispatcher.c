b5720b04e8f2be51306738aee1f347842d80ce41
Theo FERRIEUX <tferrieu@e2r2p10.42.fr>
2020-03-12 19:29:55 +0100

    Is it... finally... over? (Probably not)

--- srcs/dispatcher.c
+++ srcs/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: tferrieu <tferrieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 17:22:11 by myener            #+#    #+#             */
-/*   Updated: 2020/03/12 17:01:42 by tferrieu         ###   ########.fr       */
+/*   Updated: 2020/03/12 19:21:26 by tferrieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ static int		is_end_room(t_info *info, int room_id)
 	return (0);
 }
 
-static void		lem_in_displayhelp(int id, char *name)
+static void		lem_in_printer(int id, char *name)
 {
 	ft_putchar('L');
 	ft_putnbr(id);
@@ -75,14 +75,14 @@ void			lem_in_displayer(t_info *info, char **map)
 		a = -1;
 		while (++a < info->ant_nb)
 		{
-			if ((room_is_empty(info, info->ant[a].path[info->ant[a].i + 1])
-				|| is_end_room(info, info->ant[a].path[info->ant[a].i + 1]))
-				&& !is_end_room(info, info->ant[a].pos))
+			if (!is_end_room(info, info->ant[a].pos) &&
+				(room_is_empty(info, info->ant[a].path[info->ant[a].i + 1])
+				|| is_end_room(info, info->ant[a].path[info->ant[a].i + 1])))
 			{
 				info->ant[a].i++;
 				info->ant[a].pos = info->ant[a].path[info->ant[a].i];
 				room_name = get_room(info, info->ant[a].path[info->ant[a].i]);
-				room_name ? lem_in_displayhelp(info->ant[a].id, room_name) : 0;
+				room_name ? lem_in_printer(info->ant[a].id, room_name) : 0;
 			}
 		}
 		ft_putchar('\n');
